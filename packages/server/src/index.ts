9a0b0071459a6c0aaaa59401f169d3923202c5e6
Calle Munck <callemunck@gmail.com>
2026-09-06 20:48:10 +0200

    feat: health endpoint

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,11 @@
-export {};
+import express from "express";
+
+const app = express();
+
+app.get('/health', (_req, res ) => {
+  res.json({ ok: true });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
